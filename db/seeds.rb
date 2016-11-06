d45608b0249e4bd86f0a65af3dbb8ddd2734fda1
susan <susan.ra.evans@gmail.com>
2016-11-06 11:12:11 -0800

    file setup

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+Book.destroy_all
+
+Book.create! [
+	{ name: "Eloquent Ruby", author: "Russ Olsen" },
+	{ name: "Beginning Ruby", author: "Peter Cooper" },
+	{ name: "Metaprogramming Ruby 2", author: "Paolo Perrotta" },
+	{ name: "Design Patterns in Ruby", author: "Russ Olsen" },
+	{ name: "The Ruby Programming Language", author: "David Flanagan" }
+]
