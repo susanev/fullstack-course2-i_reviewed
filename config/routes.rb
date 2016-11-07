582f52b92dbe04aad4760f3515fdcdf94c3d03cf
susan <susan.ra.evans@gmail.com>
2016-11-07 09:36:15 -0800

    added resources and get login delete logout

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
     resources :notes, only: [:create, :destroy]
   end
 
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get "/login" => "sessions#new", as: "login"
+  delete "/logout" => "session#destroy", as: "logout"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
